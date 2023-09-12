9a1bfe861d3479f26567d4371699c90c71f8fa3d
Ouroboros-19 <nazargutsu@gmail.com>
2023-09-12 20:08:33 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿class Program
+{
+    static void Main()
+    {
+        int n = 5, jj = n - 1;
+        int[,] array = new int[n, n];
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+                if (i == j)
+                    Console.Write("{0,2}", array[i, j] = i);
+                else
+                    if (j == jj)
+                    Console.Write("{0,2}", array[i, j] = j);
+                else
+                    Console.Write("{0,2}", array[i, j]);
+            jj--;
+            Console.WriteLine();
+        }
+        Console.WriteLine("Для закрытия программы нажмите любую клавишу");
+        Console.ReadKey();
+    }
+}
